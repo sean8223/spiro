6f18176cca64fbdd06f7bf40e94c642bc28ff03b
Sean Wellington <sean8223@gmail.com>
2014-12-16 19:00:49 -0500

    Added oval function; hide toolbars when launched from home screen in iOS

--- js/spiro.js
+++ js/spiro.js
@@ -32,9 +32,32 @@ var spiro = (function($) {
 	ctx.stroke();
     }
 
+    function drawOval(ctx, x, y, len) {
+	// https://stackoverflow.com/questions/2172798/how-to-draw-an-oval-in-html5-canvas
+	var w = len;
+	var h = 20 + (len * .10);
+	var kappa = .5522848;
+	var ox = (w / 2) * kappa; // control point offset horizontal
+	var oy = (h / 2) * kappa; // control point offset vertical
+	var xe = x + w;           // x-end
+	var ye = y + h;           // y-end
+	var xm = x + w / 2;       // x-middle
+	var ym = y + h / 2;       // y-middle
+
+	ctx.beginPath();
+	ctx.moveTo(x, ym);
+	ctx.bezierCurveTo(x, ym - oy, xm - ox, y, xm, y);
+	ctx.bezierCurveTo(xm + ox, y, xe, ym - oy, xe, ym);
+	ctx.bezierCurveTo(xe, ym + oy, xm + ox, ye, xm, ye);
+	ctx.bezierCurveTo(xm - ox, ye, x, ym + oy, x, ym);
+	//ctx.closePath(); // not used correctly, see comments (use to close off open path)
+	ctx.stroke();
+    }
+
     var shapes = { "Square" : drawSquare,
 		   "Triangle" : drawTriangle,
-		   "Circle" : drawCircle };
+		   "Circle" : drawCircle,
+		   "Oval" : drawOval };
 
     function draw(e) {
 
